ff97a603bcd50dbd26e0bf87b66c430029c3ab26
DazHaze <dhayes1.wex@gmail.com>
2021-10-18 08:00:25 +0000

    add lizard and spock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,52 @@
-var choices = ["rock","paper", "scissors", "lizard", "spock"];
+var choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 for (let index = 0; index < choices.length; index++) {
     const element = choices[index];
     console.log(element);
 }
 
-var userChoice = "test";
+var userChoice = "spock";
 var computerChoice = "scissors";
 
-function CheckLogic(userChoice,computerChoice){
-    if (userChoice === computerChoice){
-        console.log("It's a tie!")
-    } else{
-        if(userChoice === "rock" && computerChoice === "paper"){
-            console.log("You Win!")
-        }else if(userChoice === "paper" && computerChoice === "rock"){
-            console.log("You Lost :(")
-        } else if(userChoice === "rock" && computerChoice === "scissors"){
-            console.log("You Win!")
-        }else if(userChoice === "scissors" && computerChoice === "rock"){
-            console.log("You Lost :(")
-        } else if(userChoice === "scissors" && computerChoice === "paper"){
-            console.log("You Win!")
-        }else if(userChoice === "paper" && computerChoice === "scissors"){
-            console.log("You Lost :(")
+function CheckLogic(userChoice, computerChoice) {
+    var winner = "";
+    if (userChoice === computerChoice) {
+        winner = "tie";
+    } else {
+        if (userChoice === "rock" && computerChoice === "paper") {
+            winner = "user";
+        } else if (userChoice === "paper" && computerChoice === "rock") {
+            winner = "computer";
+        } else if (userChoice === "rock" && computerChoice === "scissors") {
+            winner = "user";
+        } else if (userChoice === "scissors" && computerChoice === "rock") {
+            winner = "computer";
+        } else if (userChoice === "scissors" && computerChoice === "paper") {
+            winner = "user";
+        } else if (userChoice === "paper" && computerChoice === "scissors") {
+            winner = "computer";
+        } else if (userChoice === "lizard" && computerChoice === "paper") {
+            winner = "user";
+        } else if (userChoice === "paper" && computerChoice === "lizard") {
+            winner = "computer";
+        } else if (userChoice === "lizard" && computerChoice === "spock") {
+            winner = "user";
+        } else if (userChoice === "spock" && computerChoice === "lizard") {
+            winner = "computer";
+        } else if (userChoice === "spock" && computerChoice === "rock") {
+            winner = "user";
+        } else if (userChoice === "rock" && computerChoice === "spock") {
+            winner = "computer";
+        } else if (userChoice === "spock" && computerChoice === "scissors") {
+            winner = "user";
+        } else if (userChoice === "scissors" && computerChoice === "spock") {
+            winner = "computer";
         } else {
             console.log(`Choice: "${userChoice}" is not a recogized type.`);
             throw `Choice: "${userChoice}" is not a recogized type.`;
         }
     }
+    return winner;
 }
 
-CheckLogic(userChoice, computerChoice);
+console.log(CheckLogic(userChoice, computerChoice));
